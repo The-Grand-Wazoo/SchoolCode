4cfb7a987ee021bd9fd725c93e2a458c1789405a
The-Grand-Wazoo <johnsinclair16@gmail.com>
2013-02-01 23:39:02 -0500

    Class trapezoid modifies area and output so that it is specific to the trapezoid

--- InheritanceProject/src/inheritanceclassproject/JSrectangle.java
+++ InheritanceProject/src/inheritanceclassproject/JSrectangle.java
@@ -7,4 +7,4 @@ public class JSrectangle extends JSquadrilateral
         super(shapeGiven, lengthGiven, widthGiven);
         
     }
-}
+} 
--- InheritanceProject/src/inheritanceclassproject/JStrapezoid.java
+++ InheritanceProject/src/inheritanceclassproject/JStrapezoid.java
@@ -4,9 +4,9 @@ public class JStrapezoid extends JSquadrilateral
 {
     protected double base1;
     protected double base2;
-    protected double area;
-    
-    public JStrapezoid( String shapeGiven, double height, double base1Given, double base2Given, double lengthGiven, double widthGiven)
+   
+    private double height = length;
+    public JStrapezoid( String shapeGiven,  double base1Given, double base2Given, double lengthGiven, double widthGiven)
     {
         super(shapeGiven, lengthGiven, widthGiven);
         base1=base1Given;
@@ -16,11 +16,17 @@ public class JStrapezoid extends JSquadrilateral
     public double area()
     {
     	double area;
-    	double height = length;
-    	
     	area=0.5*height*(base1+base2);
 		return area;
     	
     }
     
+    public void outPut()
+    {
+    	double area=area();
+        System.out.println("The "+shape+" has a base of "+base1+", a second base of "+base2+", and a height of "+length+".");
+        System.out.println("The area of the "+shape+" is "+area+" square units.");
+    }
+    
 }
+ 
