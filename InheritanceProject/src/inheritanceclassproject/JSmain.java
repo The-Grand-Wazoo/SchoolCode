0ebd6feb62e9a981a7eba24138a28a8bdbaaac8f
The-Grand-Wazoo <johnsinclair16@gmail.com>
2013-02-01 23:34:56 -0500

    Main is finished. Calls the sub classes.

--- InheritanceProject/src/inheritanceclassproject/JSmain.java
+++ InheritanceProject/src/inheritanceclassproject/JSmain.java
@@ -2,17 +2,15 @@ package inheritanceclassproject;
 
 public class JSmain 
 {
-
-    
     public static void main(String[] args)
     {
         JSrectangle rectangle=new JSrectangle("rectangle",2.0,3.0);
-        JSsquare square=new JSsquare("square",6.0,6.0);
-        JStrapezoid trapezoid=new JStrapezoid("trapezoid",5.0,3.0,5.0,0,0);
-        trapezoid.outPut();
         rectangle.outPut();
+        JSsquare square=new JSsquare("square",6.0,6.0);
         square.outPut();
-        //System.out.println("test");
+        JStrapezoid trapezoid=new JStrapezoid("trapezoid",5.0,3.0,4.0,0);
+        trapezoid.outPut();
+       
     }
 
 }
